1a730884c273196874ee63b311e44f9d5546d280
mattiamanna2203 <mattiamanna2203@gmail.com>
2025-06-14 19:44:52 +0200

    scraper fixed
    Lo scraper ora clicca su "Carica altri risultati"

--- funzioni_selenium_bot_booking.py
+++ funzioni_selenium_bot_booking.py
@@ -27,3 +27,37 @@ def click_cookie_button(driver):
 
 
 
+# Funzioni utilizzate dal bot selenium.
+def click_caricaaltri_button(driver):
+    """
+    Questa funzione si occupa di accettare i cookies quando si accede su booking
+    """
+    try:
+        # Metodo di accettazione 1
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Carica più risultati']"))).click()
+        print("Carica altri risultati metodo 1")
+        return 
+    except:
+        pass
+    
+    try:
+        # Metodo di  accettazione 2
+        WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.de576f5064 b46cd7aad7 d0a01e3d83 dda427e6b5 bbf83acb81 a0ddd706cc"))).click()
+        print("Carica altri risultati metodo 2")
+        return 
+    except:
+        pass
+
+    try:
+        # Metodo di  accettazione 3
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Carica più risultati']"))).click()
+        print("Carica altri risultati metodo 3")
+        return 
+    except:
+        pass
+
+
+
+
+
+
